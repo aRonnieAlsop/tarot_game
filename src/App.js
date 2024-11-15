f769c645533e0461290dd3039a878b29fb5e00af
aRonnieAlsop <alsop.arron@gmail.com>
2024-11-15 12:21:54 -0800

    'hand cursor/title'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <div className="global-title">TAROT</div>
   );
 }
 
