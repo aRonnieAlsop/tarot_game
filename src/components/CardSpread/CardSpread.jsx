c96da89f061b8b00d1b50287f066cc343b99b7bc
aRonnieAlsop <alsop.arron@gmail.com>
2024-11-17 21:23:18 -0800

    'wip'

--- src/components/CardSpread/CardSpread.jsx
+++ src/components/CardSpread/CardSpread.jsx
@@ -16,18 +16,52 @@ const CardSpread = () => {
       <div className="nav-section">
         <h2 className="nav-title">Choose Your Spread</h2>
       <nav className="nav-bar">
+        {/*One Card Spread*/}
+        <div className="spread-preview">
+        <div className="spread-one-card"></div>
         <NavLink to="/one-card" className="nav-link" activeClassName="active">
           One Card
         </NavLink>
+        </div>
+        {/*Draw Three Spread*/}
+        <div className="spread-preview">
+        <div className="spread-draw-three">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+        </div>
         <NavLink to="/draw-three" className="nav-link" activeClassName="active">
           Draw Three
         </NavLink>
+        </div>
+        {/*Cross Spread*/}
+        <div className="spread-preview">
+            <div className="spread-cross">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            </div>
         <NavLink to="/cross" className="nav-link" activeClassName="active">
           Cross
         </NavLink>
+        </div>
+        {/*Horseshoe Spread*/}
+        <div className="spread-preview"> 
+            <div className="spread-horseshoe">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            </div>
         <NavLink to="/horseshoe" className="nav-link" activeClassName="active">
           Horseshoe
         </NavLink>
+        </div>
       </nav>
       </div>
     </div>
