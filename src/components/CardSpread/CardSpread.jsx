0687945ea904c79ab10e70a951a1d51af6a5f559
aRonnieAlsop <alsop.arron@gmail.com>
2024-11-24 12:06:01 -0800

    'nav-link images ✅'

--- src/components/CardSpread/CardSpread.jsx
+++ src/components/CardSpread/CardSpread.jsx
@@ -12,53 +12,35 @@ const CardSpread = () => {
             <span className="title-line">Your</span>
             <span className="title-line">Spread</span>
      </h2>
+
+     {/* Navigation Section*/}
     <div className="nav-section">
       <nav className="nav-bar">
         {/*One Card Spread*/}
         <div className="spread-preview">
-        <div className="spread-one-card"></div>
+        <img src="/tarot_assets/one_card.jpg" className="nav-image" />
         <NavLink to="/one-card" className="nav-link" activeClassName="active">
           One Card
         </NavLink>
         </div>
-        {/*Draw Three Spread*/}
         <div className="spread-preview">
-        <div className="spread-draw-three">
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-        </div>
+        <img src="/tarot_assets/draw_three.jpg" className="nav-image" />
         <NavLink to="/draw-three" className="nav-link" activeClassName="active">
           Draw Three
         </NavLink>
-        </div>
-        {/*Cross Spread*/}
         <div className="spread-preview">
-            <div className="spread-cross">
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            </div>
+        <img src="/tarot_assets/cross.jpg" className="nav-image" />
         <NavLink to="/cross" className="nav-link" activeClassName="active">
           Cross
         </NavLink>
+        
         </div>
-        {/*Horseshoe Spread*/}
         <div className="spread-preview"> 
-            <div className="spread-horseshoe">
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            </div>
+        <img src="/tarot_assets/horseshoe.jpg" className="nav-image"/>
         <NavLink to="/horseshoe" className="nav-link" activeClassName="active">
           Horseshoe
         </NavLink>
+        </div>
         </div>
       </nav>
       </div>
