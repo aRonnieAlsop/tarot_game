89c758b954bd25b6bb96ae5fbc24b059c7f4c3a4
aRonnieAlsop <alsop.arron@gmail.com>
2024-12-01 22:52:57 -0800

    'one-card-draw wip'

--- src/components/OneCardDraw/OneCardDraw.jsx
+++ src/components/OneCardDraw/OneCardDraw.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './OneCardDraw.css';
 
 const OneCardDraw = () => {
+    const [message, setMessage] = useState("Think about your question for the cards now...");
+    const [showBubble, setShowBubble] = useState(true);
+    const [showNeonSign, setShowNeonSign] = useState(false);
+
+    useEffect(() => {
+        //update message & control visibility of elements
+        const messageTimeouts = [
+            setTimeout(() => setMessage("Really focus hard on your question..."), 4000),
+            setTimeout(() => setShowBubble(false, 8000)), // Hide the bubble
+            setTimeout(() => setShowNeonSign(true), 8500), // Show neon sign
+        ];
+
+        return () => {
+            messageTimeouts.forEach(clearTimeout);
+        }
+    }, []);
+
     return (
-        <div className="landing-page">
+        <div className="one-card-draw">
           {/* Tarot Card Stack */}
           <div className="tarot-stack">
             {/* layers to mimic a stack */}
+            {/* <div className="tarot-card back-pattern"></div>
             <div className="tarot-card back-pattern"></div>
-            <div className="tarot-card back-pattern"></div>
-            <div className="tarot-card back-pattern"></div>
+            <div className="tarot-card back-pattern"></div> */}
           </div>
+
+          {/* Fortune Teller Bubble */}
+          {showBubble && (
+            <div className="fortune-bubble">
+                <p>{message}</p>
+            </div>
+          )}
+
+          {/* Neon Sign */}
+          {showNeonSign && <div className="neon-sign">Draw Card</div>}
         </div>
       );
 };
